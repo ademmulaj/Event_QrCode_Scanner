358e9f0924305532c0b36e1d5d02f8e3278c9412
adem <adem>
2024-06-06 22:17:34 +0200

    krijimi i user

---
+++ Event QrCode Scanner/Models/user.cs
@@ -0,0 +1,11 @@
+﻿namespace Event_QrCode_Scanner.Models
+{
+    public class user
+    {
+        //public int id { get; set; }
+        public string FirstLastName { get; set; }
+        public string Email { get; set; }
+        public int phoneNumber { get; set; }
+                
+    }
+}
